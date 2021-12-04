934fffd5bc681641a4a911f90aa849f7798cc665
douglas-robert-hill <douglasroberthill@gmail.com>
2021-12-04 22:13:18 +0000

    untested code

---
+++ contract.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.8.10; 
+
+
+contract PiggyBank {
+
+    address owner; 
+    uint target;
+    uint value = 0;
+    uint endDate;
+
+
+    // Initialise the contract 
+    // Define the objective saving amount and the end date
+    constructor(
+        uint targetValue, 
+        uint closeDate
+    ){
+        owner = msg.sender;
+        target = targetValue;
+        endDate = closeDate;
+
+    }
+
+
+    // Deposit some Eth in the account 
+    function deposit() public payable {
+
+        require(endDate > block.timestamp);
+
+        value = value + msg.value;
+
+    }
+
+
+    // Functions to let user see the value contained, their target and time remaining 
+    function viewAccount() public view returns(string memory, uint, string memory, uint, string memory, uint) {
+
+        return ("Value held in account: ", value, "Account target: ", target, "Closing Date: ", endDate);
+    
+    }
+
+
+    // Payout function if conditions are met
+    // If the value target has been met then payout
+    // If the timeframe has run out then payout 
+    function payout() public payable {
+
+        require(msg.sender == owner);
+
+        if (block.timestamp >= endDate) {
+            selfdestruct(payable(owner));
+        }
+
+        if (value >= target) {
+            selfdestruct(payable(owner));
+        }
+
+    }
+
+}
+
